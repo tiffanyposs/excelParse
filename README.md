7b652d51eb0c721bacdceb5006d23e720b680657
Tiffany Poss <tiffanyposs@gmail.com>
2019-10-31 13:15:29 -0400

    update functionality to use key value

--- README.md
+++ README.md
@@ -6,57 +6,36 @@ Parse excel `.xlsx` files into arrays and json objects and write them to a file.
 
 * Reads `.xlsx` files
 * Writes `.json` files
-* Can remove one row or column on request
+* Can indicate which column to use as the key
 * Catches and notifies if an error if `.xlsx` file doesn't exist or is invalid
 * Catches and notifies if requested deleted rows or columns doesn't exist in the `.xlsx` file but saves the file anyways
-
-#### Future Features
-
-* Ways to covert into an object in addition to an array
-* Choose write file type
-* Choose write file name
-* More specific row and column deleting
-
 
 ### How it works
 
 #### Arguments
 
 ```
-$ node app (root-filename) (row-deleted) (column-deleted)
+$ node app (root-filename) (index-of-column-for-key)
 
 ```
 
 #### Examples
 
-Reads `sample.xlsx` file by default and write it to `sample.json`
+Reads `sample.xlsx` file by default and uses the first column as the key
 
 ```
 $ node app
 
 ```
-Reads `test.xlsx` file that is passed in and writes it to `sample.json`
+Reads `test.xlsx` file and uses the first column as the key
 
 ```
 $ node app test
 
 ```
-Reads `test.xlsx` file and deletes the first row from the file
+Reads `test.xlsx` file and uses the 2nd column (index 1) as the key
 
 ```
 $ node app test 1
 
 ```
-Reads `test.xlsx` file and deletes the first row from the file and 2nd column
-
-```
-$ node app test 1 2
-
-```
-Reads `test.xlsx` file and deletes no rows from the file and deletes the 1st column
-
-```
-$ node app test 0 1
-
-```
-
--- app.js
+++ app.js
@@ -1,63 +1,56 @@
-var fs = require('fs');
-var parseXlsx = require('excel');
+const fs = require('fs');
+const parseXlsx = require('excel').default;
 
-var readfile = process.argv[2] ? process.argv[2] + '.xlsx' : 'sample.xlsx';
-var deleteRow = process.argv[3] ? process.argv[3] : 0;
-var deleteColumn = process.argv[4] ? process.argv[4] : 0;
+const readfile = process.argv[2] ? process.argv[2] + '.xlsx' : 'sample.xlsx';
+const key = process.argv[3] || 0;
 
-//checks if file exists
-fs.stat(readfile, function(err, stat) {
+//checks if file exists, if so it inits
+fs.stat(readfile, (err, stat) => {
     if(err == null) {
-        getData(readfile);        
+        init(readfile, key);
     } else if(err.code == 'ENOENT') {
     	console.log('That file does not exist /\nEnter an existing file name or name your file sample.xlsx');
     } else {
-        console.log('An Error Occured: ', err.code);
+      console.log('An Error Occured: ', err.code);
     }
 });
 
-//gets data from the spreadsheet
-var getData = function(filename){
-	parseXlsx(filename, function(err, data) {
-	  if(err) throw err
-	  makeArray(data, deleteRow, deleteColumn)
-	  return data
-	});
+// turns the array into an object
+const createRowObject = (row, keys) => {
+  return row.reduce((acc, cell, cellIndex) => {
+    acc[keys[cellIndex]] = cell;
+    return acc;
+  }, {});
 }
 
-//row to remove (optional)
-//column to remove (optional)
-var makeArray = function(data, row, column){
-	var row = row || 0;
-	var column = column || 0;
-	var newData = data;
-	if(!!row && !!newData[row]){
-		newData = newData.splice(row);
-	}
-	if(!newData[row]){
-		console.log("that row doesn't exist");
-	}
-	if(!!column && !!newData[0][column]){
-		newData.forEach(function(each, index){
-			var removed = each.splice(column);
-			newData[index] = removed;
-		});
-	}
-	if(!newData[0][column]){
-		console.log("that column doesn't exist");
-	}
-	writeData(newData)
-	return newData
+// creates the spreadsheet object
+const createSpreadsheetObject = (data, keys, key = 0) => {
+  return data.reduce((acc, row) => {
+    const rowData = createRowObject(row, keys);
+    const keyName = row[key];
+    acc[keyName] = rowData;
+    return acc;
+  }, {});
 }
 
+// gets data from the spreadsheet
+// transforms it into an object
+// triggers save
+const init = function(filename, key) {
+  parseXlsx(filename).then((data) => {
+    const keys = data[0];
+    data.shift(); // remove first elem from data
+    const obj = createSpreadsheetObject(data, keys, key);
+	  return writeData(obj);
+  });
+}
 
-var writeData = function(data){
-	fs.writeFile("sample.json", JSON.stringify(data), function(err) {
+// writes the object to a json file
+const writeData = data => {
+	fs.writeFile("sample.json", JSON.stringify(data), err => {
 	    if(err) {
 	        return console.log(err);
 	    }
 	    console.log("The file was saved!");
-	}); 
+	});
 }
-
-
